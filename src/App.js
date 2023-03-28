cefb75046a1d500f5b7aab424b39beea49051d27
trimeo806 <trimeo806@gmail.com>
2023-03-28 21:53:12 +0700

    last-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Controller, useForm } from "react-hook-form";
+import Typography from "@mui/material/Typography";
+import {
+  Alert,
+  IconButton,
+  InputAdornment,
+  Stack,
+  TextField,
+} from "@mui/material";
 
 function App() {
+  //Su dung react hook form tao ra {register, handleSubmit, formState: {errors}}
+  const defaultValues = {
+    email: "tringuyen@gmail.com",
+    password: "123",
+    remember: "true",
+  };
+
+  const methods = useForm({ defaultValues });
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = methods;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    setError("afterSubmit", { message: "Sever Response Error" });
+  };
+  useEffect(() => {
+    console.log(control.register);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography variant="h3" textalign="center" mb={3}>
+        h1. Heading
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={3}>
+          {!!errors.afterSubmit && (
+            <Alert severity="errors">{errors.afterSubmit.message}</Alert>
+          )}
+          <Controller
+            name="email"
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <TextField
+                label="Email Address"
+                fullWidth
+                error={!!error}
+                helperText={error?.message}
+                {...field} //Neu khong co cai nay
+                s
+              />
+            )}
+          ></Controller>
+          <Controller
+            name="password"
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <TextField
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                fullWidth
+                error={!!error}
+                helperText={error?.message}
+                {...field}
+                inputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="Toggle password visibility"
+                        onClick={() =>
+                          setShowPassword(!showPassword ? false : true)
+                        }
+                        onMouseDown={(e) => e.preventDefault()}
+                        edge="end"
+                      ></IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              ></TextField>
+            )}
+          ></Controller>
+        </Stack>
+      </form>
     </div>
   );
 }
